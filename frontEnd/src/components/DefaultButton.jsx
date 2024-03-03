d6881ccad609e00c96201204a9207c69d12e0a85
John Kyle Lampa <jklampa31@gmail.com>
2024-03-03 12:46:21 +0800

    fix props error, add contact header

--- frontEnd/src/components/DefaultButton.jsx
+++ frontEnd/src/components/DefaultButton.jsx
@@ -1,8 +1,13 @@
-// DefaultButton.jsx
-function DefaultButton({label}) {
+import PropTypes from 'prop-types';
+
+function DefaultButton({ label }) {
     return (
         <button className="default-button blue-button">{label}</button>
     );
 }
+
+DefaultButton.propTypes = {
+    label: PropTypes.string.isRequired
+};
 
 export default DefaultButton;
--- frontEnd/src/components/EmployeeTable.jsx
+++ frontEnd/src/components/EmployeeTable.jsx
@@ -7,6 +7,7 @@ function EmployeeTable({ employees }) {
                 <tr>
                     <th className="border-black border border-solid border-collapse">Employee No.</th>
                     <th className="border-black border border-solid border-collapse">Name</th>
+                    <th className="border-black border border-solid border-collapse">Contact</th>
                     <th className="border-black border border-solid border-collapse">Address</th>
                     <th className="border-black border border-solid border-collapse">Designation</th>
                     <th className="border-black border border-solid border-collapse">Employee Type</th>
@@ -20,6 +21,7 @@ function EmployeeTable({ employees }) {
                         <tr key={index}>
                             <td>{employee.employeeNo}</td>
                             <td>{employee.name}</td>
+                            <td>{employee.contact}</td>
                             <td>{employee.address}</td>
                             <td>{employee.designation}</td>
                             <td>{employee.employeeType}</td>
@@ -29,12 +31,28 @@ function EmployeeTable({ employees }) {
                     ))
                 ) : (
                     <tr>
-                        <td id="empty-list-label" colSpan={7} className="border-black border border-solid border-collapse" >No employees found</td>
+                        <td id="empty-list-label" colSpan={8} className="border-black border border-solid border-collapse" >No employees found</td>
                     </tr>
                 )}
             </tbody>
         </table>
     );
 }
+
+EmployeeTable.propTypes = {
+    employees: PropTypes.arrayOf(
+        PropTypes.shape({
+            employeeNo: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            contact : PropTypes.string.isRequired,
+            address: PropTypes.string.isRequired,
+            designation: PropTypes.string.isRequired,
+            employeeType: PropTypes.string.isRequired,
+            status: PropTypes.string.isRequired,
+            department: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
 
 export default EmployeeTable;
